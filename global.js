6ffcdb57b8df8857af610377c28f8d5acf1fc451
Xleepree <diegoed1313@gmail.com>
2025-03-22 22:58:28 -0400

    v3.1
    moonlord

--- global.js
+++ global.js
@@ -1,14 +1,17 @@
 
 // variables
 
+let points = 0; 
 let pointsPerClick = 1; // default click power
 let steroidsPrice = 100; // initial cost
 let clickCount = 0; // cps meter idle
 let cps = 0 // clicks per second
 let capybaraExists = 0;
 let monkeySlaveryActive = false;
-let monkeyPower = 1;
-let monkeyCost = 500;
+let gamblingPrice = 150;
+let fedoraActive = false;
+let monkeyInterval = null;
+let fedoraInterval = null;
 
 
 // point handling
@@ -21,13 +24,13 @@ function increasePoints() {
 }
 
 function savePoints() {
-    let points = document.getElementById("points").innerText;
-    // stuffs to save
     localStorage.setItem("points", points);
     localStorage.setItem("pointsPerClick", pointsPerClick);
     localStorage.setItem("steroidsPrice", steroidsPrice);
     localStorage.setItem("capybara", capybaraExists);
     localStorage.setItem("slave", monkeySlaveryActive);
+    localStorage.setItem("gamblingPrice", gamblingPrice);
+    localStorage.setItem("fedora", fedoraActive);
 }
 
 function loadPoints() {
@@ -38,13 +41,14 @@ function loadPoints() {
     steroidsPrice = Number(localStorage.getItem("steroidsPrice"));
     capybaraExists = localStorage.getItem("capybara");
     monkeySlaveryActive = localStorage.getItem("slave") === "true";
+    gamblingPrice = Number(localStorage.getItem("gamblingPrice"));
+    fedoraActive = localStorage.getItem("fedora") === "true";
     if (capybaraExists == 1) {
         revealCapybara();
     } else {
         hideCapybara();
     }
 
-    updateSteroidsPrice();
     updateClickPowerDisplay();
 }
 
@@ -58,8 +62,11 @@ function resetPoints() {
         monkeySlaveryActive = false;
         clearInterval(monkeyInterval);
         monkeyInterval = null;
+        gamblingPrice = 150;
+        fedoraActive = false;
+        clearInterval(fedoraInterval);
+        fedoraInterval = null;
 
-        updateSteroidsPrice();
         updateClickPowerDisplay();
         hideCapybara();
     }
@@ -67,50 +74,45 @@ function resetPoints() {
 
 // upgrade handling
 
-function injectSteroids() {
-    let points = Number(document.getElementById("points").innerText);
 
-    if (points >= steroidsPrice) {
-        document.getElementById("points").innerText = points - steroidsPrice // deduction
-        pointsPerClick *= 2;
-        steroidsPrice *= 2;
+    // steroids
 
-        updateSteroidsPrice();
-        updateClickPowerDisplay();
-    } else {
-        alert("not enough points.")
+    function injectSteroids() {
+        let points = Number(document.getElementById("points").innerText);
+        if (points >= steroidsPrice) {
+            document.getElementById("points").innerText = points - steroidsPrice // deduction
+            pointsPerClick *= 2;
+            steroidsPrice *= 2;
+            document.getElementById("steroidsPrice").innerText = `${steroidsPrice} points`;
+
+            updateClickPowerDisplay();
+        } else {
+            alert("not enough points.")
+        }
+}
+
+    // capybara
+
+    function revealCapybara() {
+        capybaraExists = 1;
+        document.getElementById("capybara").style.opacity = "0.5";
+        document.getElementById("capybaraFren").style.backgroundColor = "lightgray";
+        document.getElementById("capybaraFren").onclick = "purchased.";
     }
-}
 
-function updateSteroidsPrice() {
-    document.getElementById("steroidsPrice").innerText = `${steroidsPrice} points`;
-}
-
-function updateClickPowerDisplay() {
-    document.getElementById("clickPowerDisplay").innerText = `click power: ${pointsPerClick} points`;
-}
-
-function revealCapybara() {
-    capybaraExists = 1;
-    document.getElementById("capybara").style.opacity = "0.5";
-    document.getElementById("capybaraFren").style.backgroundColor = "lightgray";
-    document.getElementById("capybaraFren").onclick = "purchased.";
-}
-
-function hideCapybara() {
-    capybaraExists = 0;
-    document.getElementById("capybara").style.opacity = "0";
-    document.getElementById("capybaraFren").style.backgroundColor = "white";
-    document.getElementById("capybaraFren").onclick = revealCapybara;
-}
+    function hideCapybara() {
+        capybaraExists = 0;
+        document.getElementById("capybara").style.opacity = "0";
+        document.getElementById("capybaraFren").style.backgroundColor = "white";
+        document.getElementById("capybaraFren").onclick = revealCapybara;
+    }
 
     // slave
 
     function enslaveMonkey() {
         let points = Number(document.getElementById("points").innerText);
-
-        if (points >= monkeyCost) {
-            document.getElementById("points").innerText = points - monkeyCost;
+        if (points >= 500) {
+            document.getElementById("points").innerText = points - 500; // deduction
             monkeySlaveryActive = true;
             startMonkeyWork();
         } else {
@@ -125,8 +127,45 @@ function hideCapybara() {
 
         monkeyInterval = setInterval(() => {
             let points = Number(document.getElementById("points").innerText);
-            document.getElementById("points").innerText = points + monkeyPower;
+            document.getElementById("points").innerText = points + 1;
         }, 2000)
+    }
+
+    // fedora
+    
+    function purchaseFedora() {
+        let points = Number(document.getElementById("points").innerText);
+        if (points >= 5000) {
+            document.getElementById("points").innerText = points - 5000; // deduction
+            fedoraActive = true;
+            makeFedoraMagical();
+        }
+    }
+
+    function makeFedoraMagical() {
+        if (!fedoraActive) return;
+        document.getElementById("fedora").style.backgroundColor = "lightgray";
+        document.getElementById("fedora").onclick = "purchased";
+        
+        fedoraInterval = setInterval(() => {
+            let points = Number(document.getElementById("points").innerText);
+            document.getElementById("points").innerText = points + 2;
+        }, 1000)
+    }
+
+    // gambling
+
+    function beIrresponsible() {
+        let points = Number(document.getElementById("points").innerText);
+        if (points >= gamblingPrice) {
+            points = Math.random() * (points * 4);
+            gamblingPrice *= 4;
+            document.getElementById("gamblingPrice").innerText = `${gamblingPrice} points`;
+            document.getElementById("points").innerText = Math.floor(points);
+        } else {
+            alert("not enough points.")
+        }
+
     }
 
 // cps meter handling
@@ -142,3 +181,7 @@ function updateCPS() {
 }
 
 setInterval(updateCPS, 1000); // 1 second loop
+
+function updateClickPowerDisplay() {
+    document.getElementById("clickPowerDisplay").innerText = `click power: ${pointsPerClick} points`;
+}
