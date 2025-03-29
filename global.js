04c560f41e63119e6fac2206d7cd10485a553e79
Xleepree <diegoed1313@gmail.com>
2025-03-28 20:36:06 -0400

    v4.01 but real
    anyway

--- global.js
+++ global.js
@@ -3,6 +3,7 @@
 
     // main
 
+        let points = 0;
         let pointsPerClick = 1; // default click power
         let clickCount = 0; // cps meter idle
         let cps = 0 // clicks per second
@@ -13,6 +14,7 @@
         let gamblingPrice = 150;
         let stocksPrice = 750;
         let autoclickerPower = 0;
+        let autoclickerInterval = null;
 
 
 // point handling
@@ -24,45 +26,42 @@
         trackCPS();
     }
 
-    function savePoints() {
-        localStorage.setItem("points", points);
-        localStorage.setItem("pointsPerClick", pointsPerClick);
-        localStorage.setItem("steroidsPrice", steroidsPrice);
-        localStorage.setItem("capybara", capybaraExists);
-        localStorage.setItem("autoclickerPower", autoclickerPower);
-        localStorage.setItem("stocksPrice", stocksPrice);
-    }
+    // saving, loading, resetting
 
-    function loadPoints() {
-        let savedPoints = localStorage.getItem('points');
-        document.getElementById("points").innerText = savedPoints;
-        
-        pointsPerClick = Number(localStorage.getItem("pointsPerClick"));
-        steroidsPrice = Number(localStorage.getItem("steroidsPrice"));
-        capybaraExists = localStorage.getItem("capybara");
-        autoclickerPower = Number(localStorage.getItem("autoclickerPower"));
-        stocksPrice = Number(localStorage.getItem("stocksPrice"));
-
-        updateClickPowerDisplay();
-    }
-
-    function resetPoints() {
-        let confirmReset = confirm("are you sure?");
-        if (confirmReset == true) {
-            document.getElementById("points").innerText = 0;
-            pointsPerClick = 1;
-            steroidsPrice = 100;
-            autoclickerPower = 0;
-            gamblingPrice = 150;
-            stocksPrice = 750;
-
-            clearInterval(autoclickerInterval);
-            autoclickerInterval = null;
-
-            localStorage.clear();
-            updateClickPowerDisplay();
+        function save() {
+            let points = Number(document.getElementById("points").innerText);
+            localStorage.setItem("points", points);
+            localStorage.setItem("pointsPerClick", pointsPerClick);
+            localStorage.setItem("steroidsPrice", steroidsPrice);
+            localStorage.setItem("autoclickerPower", autoclickerPower);
+            localStorage.setItem("stocksPrice", stocksPrice);
         }
-    }
+
+        function load() {
+            let savedPoints = localStorage.getItem('points') || "0";
+            points = savedPoints;
+            pointsPerClick = Number(localStorage.getItem("pointsPerClick")) || 1;
+            steroidsPrice = Number(localStorage.getItem("steroidsPrice")) || 100;
+            autoclickerPower = Number(localStorage.getItem("autoclickerPower")) || 0;
+            stocksPrice = Number(localStorage.getItem("stocksPrice")) || 750;
+
+            updateClickPowerDisplay();
+            autoClick();
+        }
+
+        function reset() {
+            let confirmReset = confirm("are you sure?");
+            if (confirmReset == true) {
+                document.getElementById("points").innerText = 0;
+                pointsPerClick = 1;
+                steroidsPrice = 100;
+                autoclickerPower = 0;
+                gamblingPrice = 150;
+                stocksPrice = 750;
+
+                updateClickPowerDisplay();
+            }
+        }
 
 // upgrade handling
 
@@ -94,8 +93,8 @@
             if (points >= 500) {
                 document.getElementById("points").innerText = points - 500; // deduction
                 autoclickerPower += 10;
-                document.getElementById("monkeySlavery").style.backgroundColor = "lightgray";
-                document.getElementById("monkeySlavery").onclick = "purchased.";
+                document.getElementById("helperman").style.backgroundColor = "lightgray";
+                document.getElementById("helperman").onclick = null;
             } else {
                 alert("not enough points.")
             }
@@ -109,7 +108,7 @@
                 document.getElementById("points").innerText = points - 2500; // deduction
                 autoclickerPower += 60;
                 document.getElementById("sweatshopWorker").style.backgroundColor = "lightgray";
-                document.getElementById("sweatshopWorker").onclick = "purchased.";
+                document.getElementById("sweatshopWorker").onclick = null;
             } else {
                 alert("not enough points.")
             }
@@ -123,7 +122,7 @@
                 document.getElementById("points").innerText = points - 5000; // deduction
                 autoclickerPower += 100;
                 document.getElementById("fedora").style.backgroundColor = "lightgray";
-                document.getElementById("fedora").onclick = "purchased.";
+                document.getElementById("fedora").onclick = null;
             } else {
                 alert("not enough points.")
             }
@@ -134,7 +133,7 @@
         function stonks() {
             let points = Number(document.getElementById("points").innerText);
             if (points >= stocksPrice) {
-                points = Math.random() * (points * 5);
+                points += Math.floor(Math.random() * (points * 5));
                 stocksPrice *= 4;
                 document.getElementById("stonksPrice").innerText = `${stocksPrice} points`;
                 document.getElementById("points").innerText = Math.floor(points);
@@ -148,7 +147,7 @@
         function beIrresponsible() {
             let points = Number(document.getElementById("points").innerText);
             if (points >= gamblingPrice) {
-                points = Math.random() * (points * 2);
+                points += Math.floor(Math.random() * (points * 2));
                 gamblingPrice *= 4;
                 document.getElementById("gamblingPrice").innerText = `${gamblingPrice} points`;
                 document.getElementById("points").innerText = Math.floor(points);
@@ -166,7 +165,7 @@
                 document.getElementById("points").innerText = points - 10000000; // deduction
                 autoclickerPower += 100000;
                 document.getElementById("masterclicker").style.backgroundColor = "lightgray";
-                document.getElementById("masterclicker").onclick = "purchased.";
+                document.getElementById("masterclicker").onclick = null;
             } else {
                 alert("not enough points.")
             }
