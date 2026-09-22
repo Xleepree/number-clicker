7332810a06bba03b2f33c7e44d0df257207bded8
xleepree <xleepree@proton.me>
2026-09-21 23:11:36 -0400

    Revert "more decimal separators"
    This reverts commit c3f3136d6ea7051cc50e0a59464eb12a6243e656.

--- src/render.js
+++ src/render.js
@@ -252,7 +252,7 @@ NC.Render.achievements = function() {
 NC.Render.statPoints = function() {
     const node = NC.Render.getId(NC.RenderRegistry.elements.textfield.statPoints);
     if (!node) { return; }
-    node.innerText = NC.GameModel.core.points.toLocaleString('en-US');
+    node.innerText = NC.GameModel.core.points;
 }
 
 NC.Render.statClickPower = function() {
@@ -495,7 +495,7 @@ NC.Render.flushStoreItem = function(itemInfo) {
     } else if (object.available === true) {
         node.classList.remove("store-menu-container-unavailable");
     }
-    priceNode.innerText = object.price.toLocaleString('en-US');
+    priceNode.innerText = object.price;
 }
 
 NC.Render.gamblingStats = function() {
